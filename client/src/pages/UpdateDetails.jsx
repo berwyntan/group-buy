5b40d3cc3e11f3e55d8834187e321187e6ee3ba8
Berwyn Tan <berwyntan@gmail.com>
2022-12-07 11:05:20 +0800

    client: added cart page

---
+++ client/src/pages/UpdateDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const UserDetails = () => {
+  return (
+    <>
+        <div>Forms</div>
+        <div>update number</div>
+        <div>update name</div>
+        <div>confirm w password</div>
+    </>
+  )
+}
+
+export default UserDetails
---
+++ client/src/pages/UpdatePassword.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const UpdatePassword = () => {
+  return (
+    <>
+        <div>form</div>
+        <div>old pw</div>
+        <div>new pw</div>
+        <div>confirm new pw</div>
+    </>
+  )
+}
+
+export default UpdatePassword
