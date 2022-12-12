9eb97bdef5091b2e0157aa4d0391ae3f1739f110
Berwyn Tan <berwyntan@gmail.com>
2022-12-12 20:31:20 +0800

    added vitest

--- apps/client/tests/Account.test.jsx
+++ apps/client/tests/Account.test.jsx
@@ -1,12 +1,21 @@
 import { describe, expect, it } from "vitest";
 import { render, screen } from '@testing-library/react';
-
+import { BrowserRouter } from 'react-router-dom'
 import Account from '../src/pages/Account';
 
 describe("Account component", () => {
     it('renders greeting', () => {
-        render(<Account />)
-        screen.debug()
-        // expect(screen.getByRole('button', { name: /My Orders/})).toHaveClass('btn-primary')
+        render(<Account />, {wrapper: BrowserRouter})
+        expect(screen.getByText(/Hello/)).toBeInTheDocument()
+    })
+    it('renders Orders button', () => {
+        render(<Account />, {wrapper: BrowserRouter})
+        // screen.debug()
+        expect(screen.getByRole('button', { name: "My Orders" })).toBeInTheDocument()
+    })
+    it('renders Update Details button', () => {
+        render(<Account />, {wrapper: BrowserRouter})
+        // screen.debug()
+        expect(screen.getByRole('button', { name: "Update Account Details" })).toHaveClass('my-2')
     })
 })
